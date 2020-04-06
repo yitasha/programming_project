273764b381b39b106dd5f5f40677e96223eef765
Stanley Yeo <s3698132@student.rmit.edu.au>
2020-04-06 14:04:56 +1000

    migration of files

---
+++ stanley/loginacc.php
@@ -0,0 +1,38 @@
+<?php
+session_start();//always include this in every page, otherwise user details could not be saved
+$servername = "localhost";//check server settings for these 3 lines
+$username = "username";
+$password = "password";
+$dbname = "account";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+if($_SERVER["REQUEST_METHOD"] == "POST") {
+    // username and password sent from form 
+    
+    $myusername = mysqli_real_escape_string($db,$_POST['username']);
+    $mypassword = mysqli_real_escape_string($db,$_POST['password']);
+$sql = "SELECT userid FROM user WHERE username = '$myusername' and password = '$mypassword'";
+      $result = mysqli_query($conn,$sql);
+      $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
+      $active = $row['active'];
+      
+      $count = mysqli_num_rows($result);
+      
+      // If result matched $myusername and $mypassword, table row must be 1 row
+		
+      if($count == 1) {
+         session_register("myusername");
+         $_SESSION['login_user'] = $myusername;
+         
+         header("location: homepage.php");
+      }else {
+         $error = "Your Login Name or Password is invalid";
+      }
+    }
+$conn->close();
+?>
