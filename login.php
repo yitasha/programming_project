d6712081b2401cd763d773b30bf2e8dd6806863d
yitasha <yitashasimida@gmail.com>
2020-04-09 04:00:07 +1000

    Modified login page protection, only allow login once

--- login.php
+++ login.php
@@ -1,6 +1,16 @@
 <?php 
 session_start();
+	$username = $_SESSION['username'];
+	if(isset($username))
+	{
+		print "<script type='text/javascript'>
+		alert('You have already logged in!');
+		window.location.href = 'index.php';
+	    </script>";
+	}
+
 $page_title= "Login";
+
 
 include "./header.php"; 
 include "./navbar.php";
