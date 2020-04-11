0f34c7a5f1b98257a8cda69b5f14aa5a57d1ace7
s3657051 <s3657051@student.rmit.edu.au>
2020-04-11 15:34:29 +1000

    added css to homepage and added title, description price and links to products

--- index.php
+++ index.php
@@ -12,32 +12,282 @@ include "./navbar.php";
 
 
 <div class="container">
+	<div class="row">
 	<h2>Top Picks</h2>
-	<div class="row">
-		<div class="col-sm-4">
-			<img src="imgs/iphone.jpeg" alt="iphone" style="width:50%">
+		<div class="col-sm-3">
+			<div class="card" style="width: 18rem;">
+			<img class="card-img-top" src="imgs/iphone.jpeg" style='width:18rem;height:auto' alt="productOne">
+				<div class="card-body">
+					<h5 class="card-title">Iphone XS</h5>
+					<p class="card-text">Selling Iphone XS, in brand new condition. Phone works as intended.</p>
+					<p class="card-text">$1000</p>
+				</div>
+				<div class="card-body">
+					<a href="#" class="card-link">Buy</a>
+					<a href="#" class="card-link">Trade</a>
+				</div>
+			</div>
 		</div>
-		<div class="col-sm-4">
-		<img src="imgs/computerOne.jpg" alt="computerOne" style="width:50%">
+		<div class="col-sm-3">
+			<div class="card" style="width: 18rem;">
+			<img class="card-img-top" src="imgs/computerOne.jpg" style='width:18rem;height:auto' alt="productOne">
+				<div class="card-body">
+					<h5 class="card-title">Iphone XS</h5>
+					<p class="card-text">Selling Iphone XS, in brand new condition. Phone works as intended.</p>
+					<p class="card-text">$1000</p>
+				</div>
+				<ul class="list-group list-group-flush">
+				
+				</ul>
+				<div class="card-body">
+					<a href="#" class="card-link">Buy</a>
+					<a href="#" class="card-link">Trade</a>
+				</div>
+			</div>
 		</div>
-		<div class="col-sm-4">
-		<img src="imgs/huawei.jpg" alt="huawei" style="width:70%">
+		<div class="col-sm-3">
+			<div class="card" style="width: 18rem;">
+			<img class="card-img-top" src="imgs/iphone.jpeg" style='width:18rem;height:auto' alt="productOne">
+				<div class="card-body">
+					<h5 class="card-title">Iphone XS</h5>
+					<p class="card-text">Selling Iphone XS, in brand new condition. Phone works as intended.</p>
+					<p class="card-text">$1000</p>
+				</div>
+				<ul class="list-group list-group-flush">
+				</ul>
+				<div class="card-body">
+					<a href="#" class="card-link">Buy</a>
+					<a href="#" class="card-link">Trade</a>
+				</div>
+			</div>
+		</div>
+		<div class="col-sm-3">
+			<div class="card" style="width: 18rem;">
+			<img class="card-img-top" src="imgs/computerThree.jpeg" style='width:18rem;height:auto' alt="productOne">
+				<div class="card-body">
+					<h5 class="card-title">Iphone XS</h5>
+					<p class="card-text">Selling Iphone XS, in brand new condition. Phone works as intended.</p>
+					<p class="card-text">$1000</p>
+				</div>
+				<ul class="list-group list-group-flush">
+				</ul>
+				<div class="card-body">
+					<a href="#" class="card-link">Buy</a>
+					<a href="#" class="card-link">Trade</a>
+				</div>
+			</div>
 		</div>
 	</div>
+
+	<div class="row">
 	<h2>Recommended</h2>
-	<div class="row">
-		<div class="col-sm-4">
-		<img src="imgs/samsung.jpg" alt="samsung" style="width:80%">
+		<div class="col-sm-3">
+			<div class="card" style="width: 18rem;">
+			<img class="card-img-top" src="imgs/iphone.jpeg" style='width:18rem;height:auto' alt="productOne">
+				<div class="card-body">
+					<h5 class="card-title">Iphone XS</h5>
+					<p class="card-text">Selling Iphone XS, in brand new condition. Phone works as intended.</p>
+					<p class="card-text">$1000</p>
+				</div>
+				<div class="card-body">
+					<a href="#" class="card-link">Buy</a>
+					<a href="#" class="card-link">Trade</a>
+				</div>
+			</div>
 		</div>
-		<div class="col-sm-4">
-		<img src="imgs/computerTwo.jpeg" alt="computerOne" style="width:50%">
+		<div class="col-sm-3">
+			<div class="card" style="width: 18rem;">
+			<img class="card-img-top" src="imgs/computerOne.jpg" style='width:18rem;height:auto' alt="productOne">
+				<div class="card-body">
+					<h5 class="card-title">Iphone XS</h5>
+					<p class="card-text">Selling Iphone XS, in brand new condition. Phone works as intended.</p>
+					<p class="card-text">$1000</p>
+				</div>
+				<ul class="list-group list-group-flush">
+				
+				</ul>
+				<div class="card-body">
+					<a href="#" class="card-link">Buy</a>
+					<a href="#" class="card-link">Trade</a>
+				</div>
+			</div>
 		</div>
-		<div class="col-sm-4">
-		<img src="imgs/computerThree.jpeg" alt="computerThree" style="width:50%">
+		<div class="col-sm-3">
+			<div class="card" style="width: 18rem;">
+			<img class="card-img-top" src="imgs/iphone.jpeg" style='width:18rem;height:auto' alt="productOne">
+				<div class="card-body">
+					<h5 class="card-title">Iphone XS</h5>
+					<p class="card-text">Selling Iphone XS, in brand new condition. Phone works as intended.</p>
+					<p class="card-text">$1000</p>
+				</div>
+				<ul class="list-group list-group-flush">
+				</ul>
+				<div class="card-body">
+					<a href="#" class="card-link">Buy</a>
+					<a href="#" class="card-link">Trade</a>
+				</div>
+			</div>
+		</div>
+		<div class="col-sm-3">
+			<div class="card" style="width: 18rem;">
+			<img class="card-img-top" src="imgs/computerThree.jpeg" style='width:18rem;height:auto' alt="productOne">
+				<div class="card-body">
+					<h5 class="card-title">Iphone XS</h5>
+					<p class="card-text">Selling Iphone XS, in brand new condition. Phone works as intended.</p>
+					<p class="card-text">$1000</p>
+				</div>
+				<ul class="list-group list-group-flush">
+				</ul>
+				<div class="card-body">
+					<a href="#" class="card-link">Buy</a>
+					<a href="#" class="card-link">Trade</a>
+				</div>
+			</div>
 		</div>
 	</div>
+
+	<div class="row">
+	<h2>Accessories</h2>
+		<div class="col-sm-3">
+			<div class="card" style="width: 18rem;">
+			<img class="card-img-top" src="imgs/iphone.jpeg" style='width:18rem;height:auto' alt="productOne">
+				<div class="card-body">
+					<h5 class="card-title">Iphone XS</h5>
+					<p class="card-text">Selling Iphone XS, in brand new condition. Phone works as intended.</p>
+					<p class="card-text">$1000</p>
+				</div>
+				<div class="card-body">
+					<a href="#" class="card-link">Buy</a>
+					<a href="#" class="card-link">Trade</a>
+				</div>
+			</div>
+		</div>
+		<div class="col-sm-3">
+			<div class="card" style="width: 18rem;">
+			<img class="card-img-top" src="imgs/computerOne.jpg" style='width:18rem;height:auto' alt="productOne">
+				<div class="card-body">
+					<h5 class="card-title">Iphone XS</h5>
+					<p class="card-text">Selling Iphone XS, in brand new condition. Phone works as intended.</p>
+					<p class="card-text">$1000</p>
+				</div>
+				<ul class="list-group list-group-flush">
+				
+				</ul>
+				<div class="card-body">
+					<a href="#" class="card-link">Buy</a>
+					<a href="#" class="card-link">Trade</a>
+				</div>
+			</div>
+		</div>
+		<div class="col-sm-3">
+			<div class="card" style="width: 18rem;">
+			<img class="card-img-top" src="imgs/iphone.jpeg" style='width:18rem;height:auto' alt="productOne">
+				<div class="card-body">
+					<h5 class="card-title">Iphone XS</h5>
+					<p class="card-text">Selling Iphone XS, in brand new condition. Phone works as intended.</p>
+					<p class="card-text">$1000</p>
+				</div>
+				<ul class="list-group list-group-flush">
+				</ul>
+				<div class="card-body">
+					<a href="#" class="card-link">Buy</a>
+					<a href="#" class="card-link">Trade</a>
+				</div>
+			</div>
+		</div>
+		<div class="col-sm-3">
+			<div class="card" style="width: 18rem;">
+			<img class="card-img-top" src="imgs/computerThree.jpeg" style='width:18rem;height:auto' alt="productOne">
+				<div class="card-body">
+					<h5 class="card-title">Iphone XS</h5>
+					<p class="card-text">Selling Iphone XS, in brand new condition. Phone works as intended.</p>
+					<p class="card-text">$1000</p>
+				</div>
+				<ul class="list-group list-group-flush">
+				</ul>
+				<div class="card-body">
+					<a href="#" class="card-link">Buy</a>
+					<a href="#" class="card-link">Trade</a>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="row">
+	<h2>Laptops</h2>
+		<div class="col-sm-3">
+			<div class="card" style="width: 18rem;">
+			<img class="card-img-top" src="imgs/iphone.jpeg" style='width:18rem;height:auto' alt="productOne">
+				<div class="card-body">
+					<h5 class="card-title">Iphone XS</h5>
+					<p class="card-text">Selling Iphone XS, in brand new condition. Phone works as intended.</p>
+					<p class="card-text">$1000</p>
+				</div>
+				<div class="card-body">
+					<a href="#" class="card-link">Buy</a>
+					<a href="#" class="card-link">Trade</a>
+				</div>
+			</div>
+		</div>
+		<div class="col-sm-3">
+			<div class="card" style="width: 18rem;">
+			<img class="card-img-top" src="imgs/computerOne.jpg" style='width:18rem;height:auto' alt="productOne">
+				<div class="card-body">
+					<h5 class="card-title">Iphone XS</h5>
+					<p class="card-text">Selling Iphone XS, in brand new condition. Phone works as intended.</p>
+					<p class="card-text">$1000</p>
+				</div>
+				<ul class="list-group list-group-flush">
+				
+				</ul>
+				<div class="card-body">
+					<a href="#" class="card-link">Buy</a>
+					<a href="#" class="card-link">Trade</a>
+				</div>
+			</div>
+		</div>
+		<div class="col-sm-3">
+			<div class="card" style="width: 18rem;">
+			<img class="card-img-top" src="imgs/iphone.jpeg" style='width:18rem;height:auto' alt="productOne">
+				<div class="card-body">
+					<h5 class="card-title">Iphone XS</h5>
+					<p class="card-text">Selling Iphone XS, in brand new condition. Phone works as intended.</p>
+					<p class="card-text">$1000</p>
+				</div>
+				<ul class="list-group list-group-flush">
+				</ul>
+				<div class="card-body">
+					<a href="#" class="card-link">Buy</a>
+					<a href="#" class="card-link">Trade</a>
+				</div>
+			</div>
+		</div>
+		<div class="col-sm-3">
+			<div class="card" style="width: 18rem;">
+			<img class="card-img-top" src="imgs/computerThree.jpeg" style='width:18rem;height:auto' alt="productOne">
+				<div class="card-body">
+					<h5 class="card-title">Iphone XS</h5>
+					<p class="card-text">Selling Iphone XS, in brand new condition. Phone works as intended.</p>
+					<p class="card-text">$1000</p>
+				</div>
+				<ul class="list-group list-group-flush">
+				</ul>
+				<div class="card-body">
+					<a href="#" class="card-link">Buy</a>
+					<a href="#" class="card-link">Trade</a>
+				</div>
+			</div>
+		</div>
+	</div>
+
+
+
+
+
 </div>
 	<?php include "./footer.php"; ?>
 </body>
 </html>
+
+
 
