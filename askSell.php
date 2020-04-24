6f65c6e3e594de299662883f48090bf0faa74b12
s3657051 <s3657051@student.rmit.edu.au>
2020-04-24 18:07:25 +1000

    merged sellPage with development

--- askSell.php
+++ askSell.php
@@ -50,19 +50,6 @@ $page_title= "Sell";
         <h5><span class="askProductFont">Phone</span></h5>
       </label>
 
-<<<<<<< HEAD
-<form method="post">
-  <p>What item are you selling?:</p>
-  <input type="radio" id="phone" name="product" value="phone">
-  <label for="phone">Phone</label><br>
-  <input type="radio" id="computer" name="product" value="computer">
-  <label for="computer">Computer</label><br>
-  <input type="radio" id="laptop" name="product" value="laptop">
-  <label for="laptop">Laptop</label><br>
-  <input type="radio" id="accessories" name="product" value="accessories">
-  <label for="accessories">Accessories</label><br>
-  <input type="submit" name="nextSell" value="next" />
-=======
       <label>
         <input type="radio" name="product" value="computer">
         <img src="imgs/computerBlack-512.png" style='width:150px;height:auto;'>
@@ -84,7 +71,6 @@ $page_title= "Sell";
   <div class="row">
     <button type="submit" class="btn btn-outline-primary btn-lg " name="nextSell">Next</button>
   </div>
->>>>>>> yitasha/sellPage
 </form>
 
 	<?php include "./footer.php"?>
